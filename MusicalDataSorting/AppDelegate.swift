a64b9ef1e003c6f9e2dd64692f9501dfab9210f6
AndriusC <31574268+AndriusC@users.noreply.github.com>
2018-10-31 13:46:01 +0200

    Initial Commit

---
+++ MusicalDataSorting/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MusicalDataSorting
+//
+//  Created by Andrew on 10/31/18.
+//  Copyright © 2018 Andrew. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MusicalDataSorting/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MusicalDataSorting
+//
+//  Created by Andrew on 10/31/18.
+//  Copyright © 2018 Andrew. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
