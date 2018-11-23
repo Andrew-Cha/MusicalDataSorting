21d979f24239ddeb5f7f54c16c182073f7d65331
AndriusC <31574268+AndriusC@users.noreply.github.com>
2018-11-23 12:34:47 +0200

    Added more option buttons, more constraints

--- MusicalDataSorting/AVAudioFile Extensions/Play.swift
+++ MusicalDataSorting/AVAudioFile Extensions/Play.swift
@@ -12,23 +12,8 @@ func playFile(at url: URL, with audioPlayerNode: AVAudioPlayerNode) throws {
 		let audioFile = try AVAudioFile(forReading: url)
 		let pieces = try audioFile.splitIntoPieces(count: 10000)
 		let piecesEnumerated: [(index: Int, buffer: AVAudioPCMBuffer)] = pieces.enumerated().map { $0 }
-		
-		let selectionSorter = SelectionSort(sorting: piecesEnumerated.shuffled())
-		let timer2 = ParkBenchTimer()
-		while !selectionSorter.isDone {
-			selectionSorter.step()
-		}
-		print("Took \(timer2.stop()) seconds")
-		
-		let bubbleSorter = BubbleSort(sorting: piecesEnumerated.shuffled())
-		let timer = ParkBenchTimer()
-		while !bubbleSorter.isDone {
-			bubbleSorter.step()
-			//render
-		}
-		print("Took \(timer.stop()) seconds")
-		
-		for audioPiece in pieces.shuffled() {
+	
+		for audioPiece in pieces {
 			audioPlayerNode.scheduleBuffer(audioPiece)
 		}
 		
--- MusicalDataSorting/ViewController.swift
+++ MusicalDataSorting/ViewController.swift
@@ -2,13 +2,57 @@ import Cocoa
 import AVFoundation
 
 class ViewController: NSViewController {
+	let algorithmNames = ["Pick an Algorithm", "Bubble Sort", "Selection Sort", "This does nothing! Yeah!"]
 	@IBOutlet weak var statusLabel: NSTextField!
 	@IBOutlet weak var uploadButton: NSButton!
+	@IBOutlet weak var algorithmPopUpButton: NSPopUpButton!
+	@IBOutlet weak var playButton: NSButton!
+	@IBOutlet weak var sortButton: NSButton!
+	@IBOutlet weak var splitCountField: NSTextField!
+	@IBOutlet weak var shuffleButton: NSButton!
 	
+	var filePath: URL?
 	let audioEngine = AVAudioEngine()
 	let audioPlayer = AVAudioPlayerNode()
 	
-	@IBAction func startMusicPrompt(_ sender: NSButton) {
+	
+	@IBAction func algorithmPopUpSelected(_ sender: NSPopUpButton) {
+		let lastSelectedIndex = sender.indexOfSelectedItem
+		let lastSelectedItem = sender.item(at: lastSelectedIndex)
+		algorithmPopUpButton.setTitle(lastSelectedItem!.title)
+		sortButton.isEnabled = true
+	}
+	
+	@IBAction func playFilePrompt(_ sender: Any) {
+		do {
+			guard self.filePath != nil else { return }
+			if audioPlayer.isPlaying {
+				audioPlayer.stop()
+			}
+			try playFile(at: filePath!, with: audioPlayer)
+			self.statusLabel.stringValue = "Status - Played"
+		} catch {
+			self.statusLabel.stringValue = "Status - Playing Failed, try again"
+			self.showAlert(for: error)
+		}
+	}
+	
+	@IBAction func shufflePrompt(_ sender: Any) {
+		print("Prompted to shuffle")
+		algorithmPopUpButton.isEnabled = true
+	}
+	
+	@IBAction func splitNumberEntered(_ sender: Any) {
+		print("Value for split count entered")
+		shuffleButton.isEnabled = true
+	}
+	
+	@IBAction func sortFilePrompt(_ sender: Any) {
+		print("Prompted to sort the file")
+		sortButton.isEnabled = false
+	}
+	
+	@IBAction func startFileUploadPrompt(_ sender: NSButton) {
 		let fileSelectionPanel = NSOpenPanel()
 		fileSelectionPanel.canChooseFiles = true
 		fileSelectionPanel.allowsMultipleSelection = false
@@ -18,10 +62,14 @@ class ViewController: NSViewController {
 			guard result == .OK else { return }
 			
 			do {
-				try playFile(at: fileSelectionPanel.urls[0], with: self.audioPlayer)
-				self.statusLabel.stringValue = "Status - Successful"
+				let _ = try AVAudioFile(forReading: fileSelectionPanel.urls[0])
+				self.filePath = fileSelectionPanel.urls[0]
+				self.playButton.isEnabled = true
+				self.splitCountField.isEnabled = true
+				
+				self.statusLabel.stringValue = "Status - Uploaded"
 			} catch {
-				self.statusLabel.stringValue = "Status - Failed, try again"
+				self.statusLabel.stringValue = "Status - Upload Failed, try again"
 				self.showAlert(for: error)
 			}
 		}
@@ -29,6 +77,8 @@ class ViewController: NSViewController {
 	
 	override func awakeFromNib() {
 		super.awakeFromNib()
+		algorithmPopUpButton.removeAllItems()
+		algorithmPopUpButton.addItems(withTitles: algorithmNames)
 		
 		audioEngine.attach(audioPlayer)
 		audioEngine.connect(audioPlayer, to: audioEngine.mainMixerNode, format: nil)
